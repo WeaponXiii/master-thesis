9e6b133bd8c37d1655444fca6c6549c93ceaab4f
WeaponXiii <samaun.xiii@gmail.com>
2025-07-09 02:49:33 +0200

    Add: Method to get file content at specific commit

--- code/code-parser/code-parser/Program.cs
+++ code/code-parser/code-parser/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.CodeAnalysis.CSharp;
 using Spectre.Console;
 
 using spectre.console.helpers;
+using System.Text;
 
 MSBuildLocator.RegisterDefaults();
 
@@ -59,8 +60,8 @@ static async Task PrintSolutionInfo()
                 var currentParent = (node, parent) switch
                 {
                     (_, null) => Tree.From(node.AsNode()!),
-                    ({ IsNode: true}, IHasTreeNodes p) => p.AddNode(node.AsNode()!),
-                    ({ IsNode: false}, IHasTreeNodes p) => p.AddNode(node.AsToken()),
+                    ({ IsNode: true }, IHasTreeNodes p) => p.AddNode(node.AsNode()!),
+                    ({ IsNode: false }, IHasTreeNodes p) => p.AddNode(node.AsToken()),
                 }
 
                 /*
@@ -71,7 +72,7 @@ static async Task PrintSolutionInfo()
 
                 ;
 
-                // Recursively add child nodes
+                // Recursively process children
                 if (node.IsNode)
                 {
                     foreach (var child in node.ChildNodesAndTokens())
@@ -99,6 +100,108 @@ static async Task PrintSolutionInfo()
             AnsiConsole.Write(tree!);
         }
     }
-
+    Console.WriteLine(getFileAtCommit("6c38e51", "./code-parser/Program.cs"));
     Console.WriteLine();
+}
+
+static string getFileAtCommit(string commitHash = "6c38e51", string filePath = "./code-parser/Program.cs")
+{
+    //const int timeout = 10000; // 10 seconds
+
+    // This function is a placeholder for getting a file at a specific commit.
+    // You can implement it using LibGit2Sharp or any other Git library.
+    Console.WriteLine($"Getting file '{filePath}' at commit '{commitHash}'");
+
+    using Process p = new();
+    p.StartInfo.FileName = "/bin/bash";
+    var cmd = $"git show {commitHash}:{filePath} | cat";
+    var escapedArgs = cmd.Replace("\"", "\\\"");
+
+    //p.StartInfo.Arguments = "-c \"ls\""; // $"git show {commitHash}:{filePath} | cat";
+
+    p.StartInfo.Arguments = $"-c \"{escapedArgs}\"";
+    p.StartInfo.UseShellExecute = false;
+    p.StartInfo.RedirectStandardOutput = true;
+    p.StartInfo.RedirectStandardError = true;
+    p.StartInfo.CreateNoWindow = true;
+
+    /* StringBuilder output = new();
+    StringBuilder error = new();
+    //p.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
+    //p.ErrorDataReceived += (sender, args) => error.AppendLine(args.Data);
+
+    AutoResetEvent outputWaitHandle = new(false);
+    AutoResetEvent errorWaitHandle = new(false);
+    p.OutputDataReceived += (sender, args) =>
+    {
+        if (args.Data != null)
+        {
+            output.AppendLine(args.Data);
+        }
+        else
+        {
+            try
+            {
+                outputWaitHandle.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error setting output wait handle: {e.Message}");
+            }
+        }
+    };
+    p.ErrorDataReceived += (sender, args) =>
+    {
+        if (args.Data != null)
+        {
+            error.AppendLine(args.Data);
+        }
+        else
+        {
+            if (args.Data == null)
+            {
+                try
+                {
+                    errorWaitHandle.Set();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error setting error wait handle: {e.Message}");
+                }
+            }
+        }
+    };
+
+    p.Start();
+
+    p.BeginOutputReadLine();
+    p.BeginErrorReadLine();
+
+    if (p.WaitForExit(timeout) && outputWaitHandle.WaitOne(timeout) && errorWaitHandle.WaitOne(timeout))
+    {
+        if (!String.IsNullOrEmpty(error.ToString()))
+        {
+            return error.ToString();
+        }
+        else
+        {
+            return output.ToString();
+        }
+    }
+    else
+    {
+        throw new Exception("Program time out");
+    } */
+
+    p.Start();
+
+    string output = p.StandardOutput.ReadToEnd();
+    p.WaitForExit();
+    if (p.ExitCode != 0)
+    {
+        string error = p.StandardError.ReadToEnd();
+        throw new Exception($"Command failed with exit code {p.ExitCode}: {error}");
+    }
+
+    return output;
 }
